b362cb7fe8df086b5450a62394708ce3f5e41e48
Jordy Sipkema <jordysipkema@gmail.com>
2018-04-25 11:01:01 +0200

    Initial Commit

---
+++ TI24-OpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TI24-OpenGL
+//
+//  Created by Jordy Sipkema on 25-04-18.
+//  Copyright © 2018 Jordy Sipkema. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
